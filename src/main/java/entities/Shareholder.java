a8355cdf6509999dbb9eb6a2659c86dd4f0ff416
samangishamim <samangi624@gmail.com>
2024-01-22 15:44:17 +0330

    add needed fields

--- src/main/java/entities/Shareholder.java
+++ src/main/java/entities/Shareholder.java
@@ -1,4 +1,10 @@
 package entities;
 
+
+
 public class Shareholder {
+    private int shareholderId;
+    private String shareholderName;
+    private int nationalCode;
+    private int phoneNmber;
 }
