f07f81ed1da92c290464a6dadb76d79a020c5d9c
samangishamim <samangi624@gmail.com>
2024-01-22 15:50:30 +0330

    add constructor without id

--- src/main/java/entities/user.java
+++ src/main/java/entities/user.java
@@ -15,4 +15,10 @@ private String userName;
 private  String email;
 private  String userPasswordl;
 
+
+    public user(String userName, String email, String userPasswordl) {
+        this.userName = userName;
+        this.email = email;
+        this.userPasswordl = userPasswordl;
+    }
 }
