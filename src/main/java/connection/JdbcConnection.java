cc0b30cd7f7b59c6de88c6e784d3dfabbbcdd3d3
samangishamim <samangi624@gmail.com>
2024-01-22 14:48:44 +0330

    add postgres url for connection

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,4 +1,23 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    private static final Connection CONNECTION ;
+
+
+    static {
+        try {
+            CONNECTION = DriverManager.getConnection("jdbc:postgresql://localhost:5432/company",
+                    "postgres", "shamim1379");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getCONNECTION() {
+        return CONNECTION;
+    }
 }
