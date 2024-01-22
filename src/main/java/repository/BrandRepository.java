adf7684f77c0fc93c29505ceae60264d5da4ab41
samangishamim <samangi624@gmail.com>
2024-01-22 20:01:25 +0330

    chore: add getter and setter and  constructor for BrandRepository

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -1,13 +1,21 @@
 package repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.sql.Connection;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BrandRepository {
- private final Connection connection;
- 
+
+
 
     public BrandRepository(Connection connection) {
-        this.connection = connection;
     }
 
 }
