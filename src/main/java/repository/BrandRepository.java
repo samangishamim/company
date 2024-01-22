67601ef126b777bfb768481e14dfe1365f93335e
samangishamim <samangi624@gmail.com>
2024-01-23 01:16:20 +0330

    chore:  create saveBrand method

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -1,6 +1,10 @@
 package repository;
 
+import entities.Brand;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class BrandRepository {
     private final Connection connection;
@@ -9,4 +13,14 @@ public class BrandRepository {
         this.connection = connection;
     }
 
+    public int saveBrand(Brand brand) throws SQLException {
+        String saveBrandQuery="insert into brand(name, website, description) values (?,?,?);";
+        PreparedStatement ps=connection.prepareStatement(saveBrandQuery);
+        ps.setString(1,brand.getBrandName());
+        ps.setString(2,brand.getWebsite());
+        ps.setString(3,brand.getBrandDescription());
+
+        int result= ps.executeUpdate();
+        return result;
+    }
 }
