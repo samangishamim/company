1c5d5331eec98b612604072ed0b15de91536db42
samangishamim <samangi624@gmail.com>
2024-01-22 20:52:52 +0330

    chore: add lombok annotation

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,5 +1,12 @@
 package entities;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 
 public class User {
     private int userId;
@@ -9,17 +16,6 @@ public class User {
     private String userPasswordl;
 
 
-    public User() {
-    }
-
-    public User(int userId, String name, String userName, String email, String userPasswordl) {
-        this.userId = userId;
-        this.name = name;
-        this.userName = userName;
-        this.email = email;
-        this.userPasswordl = userPasswordl;
-    }
-
     public User(String name, String userName, String email, String userPasswordl) {
         this.name = name;
         this.userName = userName;
@@ -27,43 +23,4 @@ public class User {
         this.userPasswordl = userPasswordl;
     }
 
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserPasswordl() {
-        return userPasswordl;
-    }
-
-    public void setUserPasswordl(String userPasswordl) {
-        this.userPasswordl = userPasswordl;
-    }
 }
