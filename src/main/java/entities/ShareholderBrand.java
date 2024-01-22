c3b4029956adad41467b6679ddab27aed889bc16
samangishamim <samangi624@gmail.com>
2024-01-22 20:14:24 +0330

    chore: fix bug

--- src/main/java/entities/ShareholderBrand.java
+++ src/main/java/entities/ShareholderBrand.java
@@ -1,16 +1,18 @@
 package entities;
 
 
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 
 public class ShareholderBrand {
     private int shareholderbId;
     private int shareholderId;
     private int brandId;
+
+    public ShareholderBrand() {
+    }
+
+    public ShareholderBrand(int shareholderbId, int shareholderId, int brandId) {
+        this.shareholderbId = shareholderbId;
+        this.shareholderId = shareholderId;
+        this.brandId = brandId;
+    }
 }
