ef832f98d680fa33d9a80e20ddd5edfc422e4c06
samangishamim <samangi624@gmail.com>
2024-01-22 15:29:35 +0330

    fix brand constructor without id

--- src/main/java/entities/Brand.java
+++ src/main/java/entities/Brand.java
@@ -14,9 +14,11 @@ public class Brand {
     private int brandId;
     private String brandName;
     private String website;
+    private  String brandDescription;
 
-    public Brand(String brandName, String website) {
+    public Brand(String brandName, String website, String brandDescription) {
         this.brandName = brandName;
         this.website = website;
+        this.brandDescription = brandDescription;
     }
 }
