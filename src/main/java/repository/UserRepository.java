4a7768b0133bcb8a06d6285c3567e039175e7e7e
samangishamim <samangi624@gmail.com>
2024-01-22 22:10:16 +0330

    chore:  create signIn method

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -15,7 +15,7 @@ public class UserRepository {
     }
 
     public int SignUp(User user) throws SQLException {
-        String signUp = "insert into user(name, username, email, password) values (?,?,?,?);";
+        String signUp = "insert into \"user\"(name, username, email, password) values (?,?,?,?);";
         PreparedStatement ps = connection.prepareStatement(signUp);
         ps.setString(1, user.getName());
         ps.setString(2, user.getUserName());
@@ -26,7 +26,7 @@ public class UserRepository {
     }
 
     public boolean isUsernameExist(String username) throws SQLException {
-    String existQuery="select * from user where username=?;";
+    String existQuery="select * from \"user\" where username=?;";
         PreparedStatement ps = connection.prepareStatement(existQuery);
         ps.setString(1, username);
 
@@ -34,11 +34,20 @@ public class UserRepository {
         return resultSet.next();
     }
     public boolean isEmailExist(String email) throws SQLException {
-        String existQuery="select * from user where email=?;";
+        String existQuery="select * from \"user\" where email=?;";
         PreparedStatement ps = connection.prepareStatement(existQuery);
         ps.setString(1, email);
 
         ResultSet resultSet = ps.executeQuery();
         return resultSet.next();
+    }
+
+    public boolean SignIn(String username , String password)  throws SQLException {
+        String signIn = "select * from \"user\" where username=? and password=?;";
+        PreparedStatement ps = connection.prepareStatement(signIn);
+        ps.setString(1, username);
+        ps.setString(2, password);
+
+        return ps.executeQuery().next();
     }
 }
