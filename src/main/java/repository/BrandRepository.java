70c10d219366e825c943d397755b78ee24a8fcc3
samangishamim <samangi624@gmail.com>
2024-01-23 01:06:49 +0330

    chore:  add BrandRepository constructor

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
+import java.sql.Connection;
+
 public class BrandRepository {
+    private final Connection connection;
+
+    public BrandRepository(Connection connection) {
+        this.connection = connection;
+    }
+
 }
