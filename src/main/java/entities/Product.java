7c0629b86ec205ebaf1cb64af324c3c579c0317f
samangishamim <samangi624@gmail.com>
2024-01-22 15:40:17 +0330

    create fileds for Product class and add tostring

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,15 +1,18 @@
 package entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 
 public class Product {
 
+    private int productId;
+    private String productName;
+    private int productDate;
+    private int categoryId;
+    private int brandId;
 }
