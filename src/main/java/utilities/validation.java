3962754f267fbdf1f520e704f1ea23ea867fcf79
samangishamim <samangi624@gmail.com>
2024-01-22 18:01:41 +0330

    add isValidEmail method to check user email

--- src/main/java/utilities/validation.java
+++ src/main/java/utilities/validation.java
@@ -1,4 +1,19 @@
 package utilities;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class validation {
+    private static final Pattern PASSWORD_PATTERN ;
+    private static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+
+    public static boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    static {
+        PASSWORD_PATTERN = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])" +
+                "(?=.*[0-9])(?=.*[@#!%&*])[A-Za-z0-9@#!%&*]{8,10}$") ;
+    }
 }
