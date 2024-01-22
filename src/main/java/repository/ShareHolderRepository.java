cb7d335017e4c06cd9d812af3d104cff9679fd9e
samangishamim <samangi624@gmail.com>
2024-01-22 19:45:21 +0330

    chore: add getter setter constructor for ShareHolderRepository class

--- src/main/java/repository/ShareHolderRepository.java
+++ src/main/java/repository/ShareHolderRepository.java
@@ -1,4 +1,15 @@
 package repository;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Connection;
+@Getter
+@Setter
 public class ShareHolderRepository {
+    private final Connection connection;
+
+    public ShareHolderRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
