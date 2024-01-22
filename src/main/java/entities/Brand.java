3c49707f21f25bb40a4a299a642a08ba83a1aa6a
samangishamim <samangi624@gmail.com>
2024-01-22 20:18:04 +0330

    chore: fix bug

--- src/main/java/entities/Brand.java
+++ src/main/java/entities/Brand.java
@@ -1,18 +1,22 @@
 package entities;
 
-import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 
 public class Brand {
     private int brandId;
     private String brandName;
     private String website;
     private  String brandDescription;
+
+    public Brand() {
+    }
+
+    public Brand(int brandId, String brandName, String website, String brandDescription) {
+        this.brandId = brandId;
+        this.brandName = brandName;
+        this.website = website;
+        this.brandDescription = brandDescription;
+    }
 
     public Brand(String brandName, String website, String brandDescription) {
         this.brandName = brandName;
