9220552a01152c0113d3fa752fe0dad42f580872
samangishamim <samangi624@gmail.com>
2024-01-22 20:30:30 +0330

    chore: fix bug and re create all

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -3,22 +3,25 @@ package entities;
 
 public class User {
     private int userId;
+    private String name;
     private String userName;
     private String email;
     private String userPasswordl;
 
 
-    public User(String userName, String email, String userPasswordl) {
+    public User() {
+    }
+
+    public User(int userId, String name, String userName, String email, String userPasswordl) {
+        this.userId = userId;
+        this.name = name;
         this.userName = userName;
         this.email = email;
         this.userPasswordl = userPasswordl;
     }
 
-    public User() {
-    }
-
-    public User(int userId, String userName, String email, String userPasswordl) {
-        this.userId = userId;
+    public User(String name, String userName, String email, String userPasswordl) {
+        this.name = name;
         this.userName = userName;
         this.email = email;
         this.userPasswordl = userPasswordl;
@@ -30,6 +33,14 @@ public class User {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getUserName() {
