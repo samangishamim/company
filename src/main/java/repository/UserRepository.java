a4eb57b632a69c6d85f06e4d8610de8186146e2f
samangishamim <samangi624@gmail.com>
2024-01-22 16:06:39 +0330

    add constructor for UserRepository class

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
+import java.sql.Connection;
+
 public class UserRepository {
+    private final Connection connection;
+
+    public UserRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
