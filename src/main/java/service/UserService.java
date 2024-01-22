7f304dd7b1fe86acc78b18947291a85185b0144b
samangishamim <samangi624@gmail.com>
2024-01-22 21:27:57 +0330

    chore: get unique username

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,13 +2,38 @@ package service;
 
 import repository.UserRepository;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class UserService {
     private final UserRepository userRepository;
+    Scanner scanner = new Scanner(System.in);
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    public void SignUp() throws SQLException {
+
+        System.out.println("**** signup ****");
+        System.out.println("enter your name: ");
+        String name = scanner.nextLine();
+        String username = getUsernameUnique();
+
+
+    }
+
+    private String getUsernameUnique() throws SQLException {
+        String username;
+        while (true) {
+            System.out.println("enter your username: ");
+             username = scanner.nextLine();
+            boolean usernameExist = userRepository.isUsernameExist(username);
+            if (!usernameExist)
+                break;
+        }
+        return  username;
+    }
 
 
 }
