a44d32f3c1ff148f6a3d56add7690d570cdc0d19
samangishamim <samangi624@gmail.com>
2024-01-22 19:46:51 +0330

    chore: add getter setter constructor for ShareHolderBrandRepository class

--- src/main/java/repository/ShareHolderBrandRepository.java
+++ src/main/java/repository/ShareHolderBrandRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
+import java.sql.Connection;
+
 public class ShareHolderBrandRepository {
+    private final Connection connection;
+
+    public ShareHolderBrandRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
