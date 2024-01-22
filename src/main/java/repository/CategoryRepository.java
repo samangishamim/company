8dc43045dd68e49a7bc5f695283bd9085a6bf72e
samangishamim <samangi624@gmail.com>
2024-01-22 19:43:31 +0330

    chore: add getter setter for CategoryRepository class

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryRepository {
 }
