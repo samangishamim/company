a1d51abe3d8b99812fe3d70e3e5ecb24f2742ee7
samangishamim <samangi624@gmail.com>
2024-01-22 18:34:05 +0330

    chore: add method to check phone number shareholder

--- src/main/java/utilities/Validation.java
+++ src/main/java/utilities/Validation.java
@@ -5,10 +5,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validation {
-    private static final String PASSWORD_PATTERN="^(?=.*[a-z])(?=.*[A-Z])" +
+    private static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])" +
             "(?=.*[0-9])(?=.*[@#!%&*])[A-Za-z0-9@#!%&*]{8,10}$";
     public static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
     public static final String WEBSITE_PATTERN = "^(https?://)?([\\w-]+\\.)+[\\w-]+(/[\\w-./?%&=]*)?$";
+
+    public static final String PHONE_NUMBER_PATTERN = "^\\d{10}$";
 
     public static boolean checkEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
@@ -23,9 +25,15 @@ public class Validation {
     }
 
     public static boolean checkWebsite(String website) {
-        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Pattern pattern = Pattern.compile(WEBSITE_PATTERN);
         Matcher matcher = pattern.matcher(website);
         return matcher.matches();
 
+    }
+
+    public static boolean checkPhoneNumber(String phoneNumber) {
+        Pattern pattern = Pattern.compile(PHONE_NUMBER_PATTERN);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
     }
 }
