14ead481c46ed00c9f642c998072b871f02260bc
samangishamim <samangi624@gmail.com>
2024-01-22 23:27:47 +0330

    chore:  test sign up and sign in

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import service.UserService;
+import utilities.ApplicationContext;
 import utilities.Validation;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        UserService userService= ApplicationContext.getUserService();
+//        userService.SignUp();
+        userService.signIn();
     }
 }
