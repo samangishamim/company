cfb359673792c5edea37e873d6cee28b56f84598
samangishamim <samangi624@gmail.com>
2024-01-22 20:54:50 +0330

    chore: add signUp method in UserRepository

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,22 +1,26 @@
 package repository;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import entities.User;
 
 import java.sql.Connection;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class UserRepository {
-
+    private final Connection connection;
 
     public UserRepository(Connection connection) {
+        this.connection = connection;
     }
 
+    public int SignUp(User user) throws SQLException {
+        String signUp = "insert into userr(name, username, email, password) values (?,?,?,?);";
+        PreparedStatement ps = connection.prepareStatement(signUp);
+        ps.setString(1, user.getName());
+        ps.setString(2, user.getUserName());
+        ps.setString(3, user.getEmail());
+        ps.setString(4, user.getUserPasswordl());
 
+        return ps.executeUpdate();
+    }
 }
