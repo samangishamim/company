8f15581a80d1242a6b19f4916b8194cbd82e63f3
samangishamim <samangi624@gmail.com>
2024-01-22 15:23:53 +0330

    create GETTER,SETTER,CONSTRUCTOR for brand class

--- src/main/java/entities/Brand.java
+++ src/main/java/entities/Brand.java
@@ -1,4 +1,17 @@
 package entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Brand {
+    private int brandId;
+    private String brandName;
+    private String website;
+
 }
