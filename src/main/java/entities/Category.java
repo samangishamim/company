6b15a5f5625dc3a8ac13d75a9df00c410a49132f
samangishamim <samangi624@gmail.com>
2024-01-22 20:17:22 +0330

    chore: fix bug

--- src/main/java/entities/Category.java
+++ src/main/java/entities/Category.java
@@ -1,17 +1,19 @@
 package entities;
 
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 
 public class Category {
 private  int categoryId;
 private String categoryName;
 private  String categoryDescription;
+
+    public Category() {
+    }
+
+    public Category(int categoryId, String categoryName, String categoryDescription) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.categoryDescription = categoryDescription;
+    }
 
     public Category(String categoryName, String categoryDescription) {
         this.categoryName = categoryName;
