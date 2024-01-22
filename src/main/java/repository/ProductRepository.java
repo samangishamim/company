470773baf87fc8ad5162aa502c9ad6edfacb5b67
samangishamim <samangi624@gmail.com>
2024-01-22 19:42:53 +0330

    chore: add getter setter for ProductRepository class

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Connection;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class ProductRepository {
+
 }
