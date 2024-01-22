1a6ce58c347d999d70e398ef96a989e22c29ce57
samangishamim <samangi624@gmail.com>
2024-01-22 20:50:31 +0330

    chore: add lombok annotation

--- src/main/java/entities/ShareholderBrand.java
+++ src/main/java/entities/ShareholderBrand.java
@@ -1,42 +1,20 @@
 package entities;
 
 
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ShareholderBrand {
     private int shareholderbId;
     private int shareholderId;
     private int brandId;
 
-    public ShareholderBrand() {
-    }
-
-    public ShareholderBrand(int shareholderbId, int shareholderId, int brandId) {
-        this.shareholderbId = shareholderbId;
+    public ShareholderBrand(int shareholderId, int brandId) {
         this.shareholderId = shareholderId;
-        this.brandId = brandId;
-    }
-
-    public int getShareholderbId() {
-        return shareholderbId;
-    }
-
-    public void setShareholderbId(int shareholderbId) {
-        this.shareholderbId = shareholderbId;
-    }
-
-    public int getShareholderId() {
-        return shareholderId;
-    }
-
-    public void setShareholderId(int shareholderId) {
-        this.shareholderId = shareholderId;
-    }
-
-    public int getBrandId() {
-        return brandId;
-    }
-
-    public void setBrandId(int brandId) {
         this.brandId = brandId;
     }
 }
