e3f46c76a702d8b7137e8dbedc5848dceaee5f51
samangishamim <samangi624@gmail.com>
2024-01-23 14:50:54 +0330

    chore:  add edit method in BrandRepository

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -15,34 +15,45 @@ public class BrandRepository {
     }
 
     public int saveBrand(Brand brand) throws SQLException {
-        String saveBrandQuery="insert into brand(name, website, description) values (?,?,?);";
-        PreparedStatement ps=connection.prepareStatement(saveBrandQuery);
-        ps.setString(1,brand.getBrandName());
-        ps.setString(2,brand.getWebsite());
-        ps.setString(3,brand.getBrandDescription());
+        String saveBrandQuery = "insert into brand(name, website, description) values (?,?,?);";
+        PreparedStatement ps = connection.prepareStatement(saveBrandQuery);
+        ps.setString(1, brand.getBrandName());
+        ps.setString(2, brand.getWebsite());
+        ps.setString(3, brand.getBrandDescription());
 
-        int result= ps.executeUpdate();
+        int result = ps.executeUpdate();
         return result;
     }
 
     public Brand load(int brandId) throws SQLException {
-        String loadBrand ="select * from brand where brand_id=?;";
-        PreparedStatement ps=connection.prepareStatement(loadBrand);
-        ps.setInt(1,brandId);
+        String loadBrand = "select * from brand where brand_id=?;";
+        PreparedStatement ps = connection.prepareStatement(loadBrand);
+        ps.setInt(1, brandId);
 
         ResultSet resultSet = ps.executeQuery();
-        if (resultSet.next()){
+        if (resultSet.next()) {
             int brand_id = resultSet.getInt("brand_id");
             String name = resultSet.getString("name");
             String website = resultSet.getString("website");
             String description = resultSet.getString("description");
 
-            return new Brand(brand_id,name,website,description);
+            return new Brand(brand_id, name, website, description);
 
-        }else{
-            System.out.println("the brand id is not exist "+brandId);
+        } else {
+            System.out.println("the brand id is not exist " + brandId);
 
         }
         return null;
+    }
+
+    public int edit(Brand brand) throws SQLException {
+        String editBrand = "update brand set name=?,website=?,description=? where brand_id=?;";
+        PreparedStatement ps = connection.prepareStatement(editBrand);
+        ps.setString(1, brand.getBrandName());
+        ps.setString(2,brand.getWebsite());
+        ps.setString(3,brand.getBrandDescription());
+        ps.setInt(4,brand.getBrandId());
+
+       return ps.executeUpdate();
     }
 }
