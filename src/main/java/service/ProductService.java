104a144fb05d0ef35b36b90d61507a61fbfed2cf
samangishamim <samangi624@gmail.com>
2024-01-23 23:06:55 +0330

    chore:add addProduct method ProductService

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,11 +1,44 @@
 package service;
 
+import entities.Product;
 import repository.ProductRepository;
+import utilities.ApplicationContext;
+
+import java.sql.SQLException;
+import java.sql.Date;
+import java.util.Scanner;
 
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryService categoryService = ApplicationContext.getCategoryService();
+    private final BrandService brandService = ApplicationContext.getBrandService();
+    Scanner scanner = new Scanner(System.in);
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public void addProduct() throws SQLException {
+        System.out.println("**** add product ****");
+        System.out.println("enter product name:");
+        String productName = scanner.nextLine();
+        System.out.println("enter product create date: ");
+        String productDate = scanner.nextLine();
+        categoryService.listOfCategory();
+        System.out.println("enter category id: ");
+        int categoryId = scanner.nextInt();
+        scanner.nextLine();
+        brandService.listOfBrand();
+        System.out.println("enter brand id: ");
+        int brandId = scanner.nextInt();
+        scanner.nextLine();
+
+
+        int result = productRepository.saveProduct(new Product(productName,
+                Date.valueOf(productDate), categoryId, brandId));
+        if (result != 0) {
+            System.out.println("product is added");
+        } else
+            System.out.println("product is not added");
     }
 }
