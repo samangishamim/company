cb20a8f4e118ea7fc70bb81b657116f1d0012f9a
samangishamim <samangi624@gmail.com>
2024-01-23 16:44:11 +0330

    chore: add check website method to check

--- src/main/java/service/BrandService.java
+++ src/main/java/service/BrandService.java
@@ -1,14 +1,37 @@
 package service;
 
 import repository.BrandRepository;
+import utilities.Validation;
+
+import java.util.Scanner;
 
 public class BrandService {
 
 
     private final BrandRepository brandRepository;
+    Scanner scanner=new Scanner(System.in);
 
     public BrandService(BrandRepository brandRepository) {
         this.brandRepository = brandRepository;
     }
 
+    public void addBrand(){
+        System.out.println("*** add brand ***");
+        System.out.println("enter your brand name:");
+        String brandName = scanner.nextLine();
+
+    }
+    private String getWebsite() {
+        String website;
+        while (true) {
+            System.out.println("enter your website url: ");
+            website = scanner.nextLine();
+            boolean checkWebsite = Validation.checkWebsite(website);
+            if (checkWebsite)
+                break;
+            else
+                System.out.println("this is not correct website url");
+        }
+        return website;
+    }
 }
