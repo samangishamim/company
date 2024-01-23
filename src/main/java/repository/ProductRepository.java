3769e25c0e75188d29dd501c6896db57e476bb78
samangishamim <samangi624@gmail.com>
2024-01-23 16:09:44 +0330

    chore: add saveProduct method in ProductRepository for saving

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,4 +1,27 @@
 package repository;
 
+import entities.Brand;
+import entities.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ProductRepository {
+    private final Connection connection;
+
+    public ProductRepository(Connection connection) {
+        this.connection = connection;
+    }
+    public int saveProduct(Product product) throws SQLException {
+        String savePrandQuery = "insert into product(name, create_date, category_id, brand_id) values (?,?,?,?);";
+        PreparedStatement ps = connection.prepareStatement(savePrandQuery);
+        ps.setString(1,product.getProductName() );
+        ps.setInt(2,product.getProductDate());
+        ps.setInt(3, product.getCategoryId());
+        ps.setInt(4,product.getBrandId());
+
+        int result = ps.executeUpdate();
+        return result;
+    }
 }
