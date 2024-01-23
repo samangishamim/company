d033438d658f55076bb38c510095f7bc6ca0e3c2
samangishamim <samangi624@gmail.com>
2024-01-23 16:26:23 +0330

    chore: add saveCategory method

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,4 +1,25 @@
 package repository;
 
+import entities.Brand;
+import entities.Category;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CategoryRepository {
+    private final Connection connection;
+
+    public CategoryRepository(Connection connection) {
+        this.connection = connection;
+    }
+    public int saveCategory (Category category) throws SQLException {
+        String saveCategoryQuery = "insert into category(name,description)values (?,?);";
+        PreparedStatement ps = connection.prepareStatement(saveCategoryQuery);
+        ps.setString(1, category.getCategoryName());
+        ps.setString(2, category.getCategoryDescription());
+
+        int result = ps.executeUpdate();
+        return result;
+    }
 }
