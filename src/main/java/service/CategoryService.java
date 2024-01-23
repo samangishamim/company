af873d52db2a25c3c9589758b26e78ea9a1f1c22
samangishamim <samangi624@gmail.com>
2024-01-23 16:21:31 +0330

    chore: add constructor

--- src/main/java/service/CategoryService.java
+++ src/main/java/service/CategoryService.java
@@ -3,6 +3,9 @@ package service;
 import repository.CategoryRepository;
 
 public class CategoryService {
-    public CategoryService(CategoryRepository categoryRepository) {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository1) {
+        this.categoryRepository = categoryRepository1;
     }
 }
