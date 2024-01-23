68c9183850409b3729f2ba9a474176806b6c5104
samangishamim <samangi624@gmail.com>
2024-01-23 23:24:08 +0330

    chore:test main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,36 @@
 import service.BrandService;
+import service.CategoryService;
+import service.ProductService;
 import service.UserService;
 import utilities.ApplicationContext;
 import utilities.Validation;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws SQLException {
+        Scanner scanner=new Scanner(System.in);
+
 //        UserService userService= ApplicationContext.getUserService();
 //        userService.SignUp();
 //        userService.signIn();
 //        BrandService brandService = ApplicationContext.getBrandService();
 //        brandService.addBrand();
-//        brandService.deleteBrand();
+//        System.out.println("enter brand id to edit");
+//        int brandId = scanner.nextInt();
+//        brandService.deleteBrand(brandId);
+
+//        scanner.nextLine();
+//        brandService.editBrand(brandId);
+//        brandService.listOfBrand();
+
+//        ProductService productService=ApplicationContext.getProductService();
+//        productService.addProduct();
+
+//        CategoryService categoryService=ApplicationContext.getCategoryService();
+//        categoryService.addCategory();
+
     }
 }
