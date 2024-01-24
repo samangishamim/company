bc33ebc35ceb61370bb223d79ae3228477e4087c
samangishamim <samangi624@gmail.com>
2024-01-25 01:05:58 +0330

    chore: add deletProduct method

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,6 +1,6 @@
 package service;
 
-import entities.Brand;
+import entities.Category;
 import entities.Product;
 import repository.ProductRepository;
 import utilities.ApplicationContext;
@@ -24,8 +24,10 @@ public class ProductService {
         System.out.println("**** add product ****");
         System.out.println("enter product name:");
         String productName = scanner.nextLine();
-        System.out.println("enter product create date: ");
-        String productDate = scanner.nextLine();
+
+
+        String productDate = getValidDate();
+
         categoryService.listOfCategory();
         System.out.println("enter category id: ");
         int categoryId = scanner.nextInt();
@@ -53,30 +55,50 @@ public class ProductService {
         System.out.println("your product name is: " + product.getProductName());
         String productName = scanner.nextLine();
         System.out.println("your product date is: " + product.getProductDate());
-        Date productDate = scanner.nextInt();
+        String productDate = scanner.next();
         System.out.println("your category id is: " + product.getCategoryId());
         int productCategoryId = scanner.nextInt();
         System.out.println("your brand id is: " + product.getBrandId());
         int productBrandId = scanner.nextInt();
 
 
-        int result = productRepository.editProduct(new Product(productId, productName, productDate, productCategoryId, productBrandId));
+        int result = productRepository.editProduct(new Product(productId, productName, Date.valueOf(productDate), productCategoryId, productBrandId));
         if (result != 0) {
             System.out.println("edit is done");
         } else
             System.out.println("error -edit");
     }
 
-    private Date getValidDate() {
-        Date date = Date.valueOf("");
+    private String getValidDate() {
+       String date;
         while (true) {
-            System.out.println("Enter the date (in the format MM/DD/YYYY):");
-            date = Date.valueOf(scanner.nextLine());
+            System.out.println("Enter the date (in the format YYYY-MM-DD):");
+            date = scanner.nextLine();
             if (Validation.checkValidDate(date)) {
                 break;
             } else
                 System.out.println("invalid date");
         }
         return date;
+    }
+    public void deletProduct(int productId) throws SQLException {
+        System.out.println("**** delete product *****");
+        Product product= productRepository.findProductById(productId);
+        System.out.println(product);
+        System.out.println("want to delete this product: [y / n]");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toLowerCase();
+        if (input.equals("y")) {
+            int delete = productRepository.deleteProduct(productId);
+            if (delete != 0)
+                System.out.println("product deleted successfully.");
+            else
+                System.out.println("error");
+        } else if (input.equals("n")) {
+            System.out.println("product not deleted.");
+        } else {
+            System.out.println("Invalid input. Please enter 'y' or 'n'.");
+        }
+
     }
 }
