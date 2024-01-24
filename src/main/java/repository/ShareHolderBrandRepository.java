e27351ef3282155df746eb28111ff206ffd398e9
samangishamim <samangi624@gmail.com>
2024-01-24 22:41:06 +0330

    chore: add saveShareholderBrand method in ShareHolderBrandRepository

--- src/main/java/repository/ShareHolderBrandRepository.java
+++ src/main/java/repository/ShareHolderBrandRepository.java
@@ -1,7 +1,27 @@
 package repository;
 
+import entities.Shareholder;
+import entities.ShareholderBrand;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ShareHolderBrandRepository {
+    private final Connection connection;
+
+    public ShareHolderBrandRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+    public  int saveShareholderBrand(ShareholderBrand shareholderBrand) throws SQLException {
+        String addShareholderBrandQuery = "insert into shareholder_brand ( shareholder_id, brand_id)values (?,?);";
+        PreparedStatement ps = connection.prepareStatement(addShareholderBrandQuery);
+        ps.setInt(1, shareholderBrand.getShareholderId());
+        ps.setInt(2,shareholderBrand.getBrandId());
+
+        int result = ps.executeUpdate();
+        return result;
+    }
 
 }
