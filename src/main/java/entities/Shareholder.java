3e306678dc0c8a94b0df84aab792efc4d68283bf
samangishamim <samangi624@gmail.com>
2024-01-24 18:54:55 +0330

    chore: fix shareholder fields

--- src/main/java/entities/Shareholder.java
+++ src/main/java/entities/Shareholder.java
@@ -11,20 +11,23 @@ public class Shareholder {
     private int shareholderId;
     private String shareholderName;
     private int nationalCode;
-    private int phoneNumber;
+    private String phoneNumber;
     private Brand[] brands;
 
 
-    public Shareholder(String shareholderName, int nationalCode, int phoneNumber) {
+    public Shareholder(String shareholderName, int nationalCode, String phoneNumber) {
         this.shareholderName = shareholderName;
         this.nationalCode = nationalCode;
         this.phoneNumber = phoneNumber;
     }
 
-    public Shareholder(int shareholderId, String shareholderName, int nationalCode, int phoneNumber) {
+    public Shareholder(int shareholderId, String shareholderName, int nationalCode, String phoneNumber) {
         this.shareholderId = shareholderId;
         this.shareholderName = shareholderName;
         this.nationalCode = nationalCode;
         this.phoneNumber = phoneNumber;
     }
+
+
+
 }
