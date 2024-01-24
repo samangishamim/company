909f15dd4368488a37b4306c4c52fa59c080c020
samangishamim <samangi624@gmail.com>
2024-01-24 23:46:50 +0330

    chore: add  constructor ShareholderBrandService

--- src/main/java/service/ShareholderBrandService.java
+++ src/main/java/service/ShareholderBrandService.java
@@ -1,4 +1,11 @@
 package service;
 
+import repository.ShareholderBrandRepository;
+
 public class ShareholderBrandService {
+    private final ShareholderBrandRepository shareholderBrandRepository;
+
+    public ShareholderBrandService(ShareholderBrandRepository shareholderBrandRepository) {
+        this.shareholderBrandRepository = shareholderBrandRepository;
+    }
 }
