d3bd7f5205e78db468559fc23865d162916cdf2a
samangishamim <samangi624@gmail.com>
2024-01-24 23:42:51 +0330

    chore: add new constructor ShareholderBrandRepository

--- src/main/java/repository/ShareholderBrandRepository.java
+++ src/main/java/repository/ShareholderBrandRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
+import java.sql.Connection;
+
 public class ShareholderBrandRepository {
+    private  final Connection connection;
+
+
+    public ShareholderBrandRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
