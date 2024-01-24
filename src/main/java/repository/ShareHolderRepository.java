c04424853429c26f806dc0d43190baea7aa510ef
samangishamim <samangi624@gmail.com>
2024-01-24 13:17:47 +0330

    chore:t add addShareholder method to add shareholder

--- src/main/java/repository/ShareHolderRepository.java
+++ src/main/java/repository/ShareHolderRepository.java
@@ -1,9 +1,14 @@
 package repository;
 
+import entities.Brand;
+import entities.Shareholder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 @Getter
 @Setter
 public class ShareHolderRepository {
@@ -12,4 +17,16 @@ public class ShareHolderRepository {
     public ShareHolderRepository(Connection connection) {
         this.connection = connection;
     }
+
+    public  int addShareholder(Shareholder shareholder) throws SQLException {
+        String addShareholderQuery = "insert into shareholder (name, national_code, phone_number) values (?,?,?);";
+        PreparedStatement ps = connection.prepareStatement(addShareholderQuery);
+        ps.setString(1, shareholder.getShareholderName());
+        ps.setInt(2, shareholder.getNationalCode());
+        ps.setInt(3, shareholder.getPhoneNumber());
+
+        int result = ps.executeUpdate();
+        return result;
+    }
+
 }
