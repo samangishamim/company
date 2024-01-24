88739dbdbdefa1eba03932ac7eb4db8d66fdb409
samangishamim <samangi624@gmail.com>
2024-01-24 13:49:13 +0330

    chore: add addShareholder method in service

--- src/main/java/service/ShareHolderService.java
+++ src/main/java/service/ShareHolderService.java
@@ -1,10 +1,35 @@
 package service;
 
+import entities.Brand;
+import entities.Shareholder;
 import repository.ShareHolderRepository;
+
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ShareHolderService {
     private final ShareHolderRepository shareHolderRepository;
+    Scanner scanner=new Scanner(System.in);
     public ShareHolderService( ShareHolderRepository shareHolderRepository1) {
         this.shareHolderRepository = shareHolderRepository1;
     }
+    public void addShareholder() throws SQLException {
+        System.out.println("*** add shareholder ***");
+        System.out.println("shareholder enter your name : ");
+        String shareholderName = scanner.nextLine();
+        System.out.println("enter your national code: ");
+        int nationalCode = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("enter your phonenumber: ");
+        int phoneNumber = scanner.nextInt();
+        scanner.nextLine();
+
+
+        int result = shareHolderRepository.saveShareholder(new Shareholder(shareholderName,nationalCode,phoneNumber));
+        if (result != 0) {
+            System.out.println("new shareholder  has been added ");
+        } else
+            System.out.println("error");
+    }
+
 }
